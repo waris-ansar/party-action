752c8d377b19fcf6f96edb1c79a235501004ddb5
Masood ul Rehman <masooddulrehman@gmail.com>
2023-09-01 21:12:41 +0500

    fixed full width issue , removed unused packages

--- components/Mainbody.jsx
+++ components/Mainbody.jsx
@@ -7,7 +7,7 @@ import Contact from "./contentbody/Contact";
 
 function Mainbody({ active, setActiveSection }) {
   return (
-    <div className="absolute top-0 left-20 bottom-0 right-0 h-[100vh]  overflow-hidden">
+    <div className="absolute top-0 left-20 bottom-0 right-0   overflow-hidden">
       <Home active={active} />
       <Work active={active} setActiveSection={setActiveSection} />
       <Schedule active={active} setActiveSection={setActiveSection} />
--- pages/index.js
+++ pages/index.js
@@ -5,7 +5,7 @@ import Mainbody from "../components/Mainbody";
 export default function Home() {
   const [activeSection, setActiveSection] = useState("home");
   return (
-    <div className="flex h-screen w-full bg-black">
+    <div className="flex h-screen w-full bg-black ">
       <Header active={activeSection} setActiveSection={setActiveSection} />
       <Mainbody active={activeSection} setActiveSection={setActiveSection} />
     </div>
